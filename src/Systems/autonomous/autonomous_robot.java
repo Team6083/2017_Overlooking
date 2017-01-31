c8f0fb5a0aeb2243fba13bd5c80bf8184da1f9d4
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-01-31 12:03:00 +0800

    add gyro_control.init()

--- src/Systems/autonomous/autonomous_robot.java
+++ src/Systems/autonomous/autonomous_robot.java
@@ -1,5 +1,14 @@
 package Systems.autonomous;
 
+import Systems.autonomous.gyro_control;
+import Systems.DriveBase;
+
 public class autonomous_robot {
 	
+	
+	
+	//init code when robot during autonomous mode
+	public static void init(){
+		gyro_control.init();
+	}
 }
