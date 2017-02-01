924b0a75bb8ea98aa8104c0c3dcfcd62939e0449
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-01 10:49:18 +0800

    add first_run

--- src/Systems/autonomous/autonomous_robot.java
+++ src/Systems/autonomous/autonomous_robot.java
@@ -6,7 +6,7 @@ import Systems.DriveBase;
 
 public class autonomous_robot {
 	
-	
+	private static boolean first_run = false;
 	
 	//init code when robot during autonomous mode
 	public static void init(){
@@ -15,11 +15,12 @@ public class autonomous_robot {
 	
 	public static void loop(){
 		gyro_control.rotate();
-		if(gyro_control.isTargetangle){
+		if(gyro_control.isTargetangle||first_run){
 			gyro_control.toangle = 30;
 			Timer.delay(5);
 		}
 		DriveBase.input_control();
 		DriveBase.input(gyro_control.left_speed, gyro_control.right_speed);
+		if(first_run) first_run = false;
 	}
 }
