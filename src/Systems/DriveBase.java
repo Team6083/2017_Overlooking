3490fa388105e85564c02930572f7bafddd4f7db
alex <alexabc722@gmail.com>
2017-02-09 17:26:20 +0800

    add arcade control

--- src/Systems/DriveBase.java
+++ src/Systems/DriveBase.java
@@ -7,25 +7,25 @@ import Systems.Joysticks;
 public class DriveBase {
     private static VictorSP left1,left2,right1,right2;
     private static double left_speed,right_speed;
+
+    private static final int left1_port = 1;
+    private static final int left2_port = 2;
+    private static final int right1_port = 3;
+    private static final int right2_port = 4;
     
-    private static final int left1_port = 0;
-    private static final int left2_port = 1;
-    private static final int right1_port = 2;
-    private static final int right2_port = 3;
-    
-    private static double speed_dawn = 4;
+    private static double speed_down = 4;
     
     public static void init(){
     	left1 = new VictorSP(left1_port);
     	left2 = new VictorSP(left2_port);
     	right1 = new VictorSP(right1_port);
     	right2 = new VictorSP(right2_port);
-        SmartDashboard.putNumber("Drive speed dawn vlaue", speed_dawn);
+        SmartDashboard.putNumber("Drive speed dawn vlaue", speed_down);
     }
     
     public static void tankDrive(){
-    	left_speed = -Joysticks.ly/speed_dawn;
-    	right_speed = Joysticks.ry/speed_dawn;
+    	left_speed = -Joysticks.ly/speed_down;
+    	right_speed = Joysticks.ry/speed_down;
     	
     	if(Joysticks.lab) left_speed = left_speed*2;
     	if(Joysticks.rab) right_speed = right_speed*2;
@@ -41,5 +41,30 @@ public class DriveBase {
     private static void dashboard(){
     	SmartDashboard.putNumber("left_drive", left1.get());
     	SmartDashboard.putNumber("right_drive", right1.get());
+    }
+    
+    public static void arcade(){
+    	
+    	right_speed=(-Joysticks.ly+Joysticks.lx)/speed_down;
+    	left_speed=(Joysticks.ly+Joysticks.ly)/4;
+    	 /* if(Joysticks.lb){
+    			ballmotor.set(ballspeed);
+    	  }else if(Joysticks.rb){
+    			ballmotor.set(-ballspeed);
+    	  }else{
+    		  ballmotor.set(0);		
+    	  }*/
+    	
+    if(Joysticks.lab){
+    	left_speed=left_speed*2;
+    right_speed=right_speed*2;
+    }
+	left1.set(left_speed);
+	left2.set(left_speed);
+	right1.set(right_speed);
+	right2.set(right_speed);
+//	SmartDashboard.putNumber("ballmotor", ballmotor.get());
+	SmartDashboard.putNumber("leftmotor", left1.get());
+	SmartDashboard.putNumber("rightmotor", right1.get());
     }
 }
