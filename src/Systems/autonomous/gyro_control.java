1273c4c3c40ca1063ff7876b3c70ddbb6d81eab5
alex <alexabc722@gmail.com>
2017-02-10 14:45:04 +0800

    change angle to error_angle

--- src/Systems/autonomous/gyro_control.java
+++ src/Systems/autonomous/gyro_control.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class gyro_control {
     private static ADXRS450_Gyro Gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
-    private static double angle=0,x=0.01,curr=0;
+    private static double error_angle=0,x=0.01,curr=0;
     private static double error_range=5,max_speed=0.25;
 	
     public static double left_speed=0,right_speed=0,toangle=0;
@@ -29,7 +29,7 @@ public class gyro_control {
     }
     
     public static void init(){
-        SmartDashboard.putNumber("angle", angle);
+        SmartDashboard.putNumber("error_angle", error_angle);
         SmartDashboard.putNumber("x", x);
         SmartDashboard.putNumber("error_range", error_range);
         SmartDashboard.putNumber("max_speed",max_speed);
@@ -50,41 +50,39 @@ public class gyro_control {
     
     public static void loop(double to){
     	//left is - right is +
-        angle = Gyro.getAngle()-to;
+        error_angle = Gyro.getAngle()-to;
         SmartDashboard.putNumber("current angle", Gyro.getAngle());
         error_range = SmartDashboard.getNumber("error_range");
         max_speed = SmartDashboard.getNumber("max_speed");
         x = SmartDashboard.getNumber("x");
-        if(angle >=360){
-        	do{
-        		angle=angle-360;
-        	}while(angle>0);
+        if(error_angle >=360){
+        	error_angle=error_angle%360;
         }
-        else if(angle <=-360){
-        	do{
-        		angle=angle+360;
-        	}while(angle<0);
+        else if(error_angle <=-360){
+        	error_angle=-error_angle;
+        	error_angle=error_angle%360;
+        	error_angle=-error_angle;
         }//make the error angle not exceed 360
         
         
-        if(angle<=-error_range && angle >=-(360-error_range)){
-        	if(angle*x <=-max_speed){
+        if(error_angle<=-error_range && error_angle >=-(360-error_range)){
+        	if(error_angle*x <=-max_speed){
         		right_speed = -max_speed;//-  turn right
         		left_speed = -max_speed;
         	}//limit the speed
         	else{
-        		right_speed = angle*x;
-        		left_speed = angle*x;
+        		right_speed = error_angle*x;
+        		left_speed = error_angle*x;
         	}
         }
-        else if(angle >=error_range && angle <=(360-error_range)){
-        	if(angle*x >= max_speed){
+        else if(error_angle >=error_range && error_angle <=(360-error_range)){
+        	if(error_angle*x >= max_speed){
         		left_speed = max_speed;
         		right_speed = max_speed;
         	}//limit the speed
         	else{
-        		right_speed = angle*x;
-        		left_speed = angle*x;
+        		right_speed = error_angle*x;
+        		left_speed = error_angle*x;
         	}
         }
         else{
@@ -93,7 +91,7 @@ public class gyro_control {
         	isTargetangle = true;
         }
         
-        SmartDashboard.putNumber("angle", angle);
+        SmartDashboard.putNumber("error_angle", error_angle);
     }
     
     
