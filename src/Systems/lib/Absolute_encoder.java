1502c1ae989dc65b4e771917542b651eb8bbcd97
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-14 23:00:29 +0800

    read_step finished

--- src/Systems/lib/Absolute_encoder.java
+++ src/Systems/lib/Absolute_encoder.java
@@ -9,18 +9,29 @@ public class Absolute_encoder {
 	
 	private static final int min_step = 0;
 	private static final int max_step = 4096;//needs to check out the value
+	private static int lap_count_value = 4000;
 	
 	private int step,prev_step;//the step of this lap
-	private int lap;
+	private int accumulation_step;
 	
 	Absolute_encoder(int port){
 		encoder_port = port;
 		encoder = new AnalogInput(encoder_port);
+		prev_step = encoder.getValue()-min_step;
+		accumulation_step = 0;
+		step = 0;
 	}	
 	
-	public void read_distence(){
-		step = encoder.getValue();
-		
-		
+	public void read_step(){
+		step = encoder.getValue() - min_step;
+		if(step-prev_step > -lap_count_value){
+			accumulation_step = accumulation_step + step;
+		}//forward count lap
+		else if(step-prev_step > lap_count_value){
+			accumulation_step = accumulation_step - (max_step - step);
+		}//backward count lap
+		else{
+			accumulation_step = accumulation_step + step - prev_step;
+		}//in same lap
 	}
 }
