05935d35c8f003861f617c6af2249f7227141ed4
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-16 01:08:09 +0800

    finish walk_distence()

--- src/Systems/encoder.java
+++ src/Systems/encoder.java
@@ -3,16 +3,21 @@ package Systems;
 import Systems.lib.Absolute_encoder;
 
 public class encoder {
-	private static int error_step,target_step=0;
+	private static int error_step_left,error_step_right,target_step=0;
+	private static double x = 0.00005;
 	
 	private static Absolute_encoder left,right;
 	
-	private static final int wheel_circumference = 0;//needs to meaesure
+	private static final int wheel_circumference = 0;//needs to measure
 	
 	private static final int left_port = 0;
 	private static final int right_port = 1;
 	
+	private static int error_range = 10;
+	private static double max_speed = 0.6;
+	
 	public static boolean isTargetdistence = false;
+	public static double left_speed=0,right_speed=0;
 	
 	public static void init(){
 		left = new Absolute_encoder(left_port);
@@ -22,15 +27,43 @@ public class encoder {
 	public static void loop(){
 		left.loop();
 		right.loop();
-		
+		walk_distence();
 	}
 	
 	public static void walk_distence(){
+		error_step_left = left.get_distence() - target_step;
+		error_step_right = right.get_distence() - target_step;
+		
+		if((int) Math.abs(error_step_left) > error_range){
+			left_speed = (error_step_left * x);
+		}
+		else left_speed = 0;
+		
+		if((int) Math.abs(error_step_right) > error_range){
+			right_speed = error_step_right * x;
+		}
+		else right_speed = 0;
+		
+		if(Math.abs(left_speed) > max_speed){
+			if(left_speed > 0) left_speed = max_speed;
+			else left_speed = -max_speed;
+			isTargetdistence = false;
+		}
+		else if(left_speed == 0) isTargetdistence = true;
+		
+		if(Math.abs(right_speed) > max_speed){
+			if(right_speed > 0) right_speed = max_speed;
+			else right_speed = -max_speed;
+			isTargetdistence = false;
+		}
+		else if(right_speed == 0) isTargetdistence = true;
 		
 	}
 	
     public static void set_to(double distence){//for inches
     	double temp = distence / wheel_circumference;
     	target_step = (int) (temp * Absolute_encoder.max_step);
+    	left.set_start_step();
+    	right.set_start_step();
     }
 }
