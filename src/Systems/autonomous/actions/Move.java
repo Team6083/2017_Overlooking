eddc94f2c10523ba79178ed05886c939f81f0e25
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-17 11:22:22 +0800

    add action move

---
+++ src/Systems/autonomous/actions/Move.java
@@ -0,0 +1,42 @@
+package Systems.autonomous.actions;
+
+import Systems.encoder;
+import Systems.DriveBase;
+
+public class Move implements Action {
+	
+	private double distance;
+	
+	private boolean started = false;
+	
+	@Override
+	public void loop() {
+		if(!started) return;
+		
+		encoder.loop();
+		DriveBase.input(encoder.left_speed, encoder.right_speed);
+		DriveBase.drivabase_control();
+	}
+
+	@Override
+	public void start() {
+		started = true;
+		encoder.set_to(distance);
+		DriveBase.mode_selector(1);
+	}
+
+	@Override
+	public void done() {
+		DriveBase.input(0, 0);
+	}
+
+	@Override
+	public boolean isFinished() {
+		return encoder.isTargetdistance;
+	}
+	
+	public Move(double temp){
+		distance = temp;
+	}
+
+}
