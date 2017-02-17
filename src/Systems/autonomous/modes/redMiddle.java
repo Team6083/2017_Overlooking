5eaf902838796319aa0c590c6e71f2054c9660cf
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-17 11:24:37 +0800

    add goToAirShip action to redMiddle

--- src/Systems/autonomous/modes/redMiddle.java
+++ src/Systems/autonomous/modes/redMiddle.java
@@ -1,15 +1,33 @@
 package Systems.autonomous.modes;
 
-import Systems.encoder;
+import Systems.autonomous.actions.*;
 
 public class redMiddle {
-
+	
+	private static Move goToAirShip;
+	
+	private static final int action_count = 3;
+	private static boolean[] finished = new boolean[action_count];
+	private static int index = 0;
 	
 	public static void init(){
+		goToAirShip = new Move(110);// put distance to airship
 		
+		for(int i=0;i<action_count;i++){
+			finished[i] = false;
+		}
+		
+		goToAirShip.start();
 	}
 	
 	public static void loop(){
+		if(index == 0){
+			goToAirShip.loop();
+			if(goToAirShip.isFinished()) index++;
+		}
+		else if(index == 1){
+			
+		}
 		
 	}
 	
