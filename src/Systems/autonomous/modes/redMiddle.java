c999a436708a894af9505f7992ec67a3fc1ac7db
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-17 12:00:48 +0800

    redMiddle mode update

--- src/Systems/autonomous/modes/redMiddle.java
+++ src/Systems/autonomous/modes/redMiddle.java
@@ -4,29 +4,54 @@ import Systems.autonomous.actions.*;
 
 public class redMiddle {
 	
-	private static Move goToAirShip;
+	private static Move goToAirShip,walkbackfromAirShip;
+	private static Stop stopAtAirShip;
 	
 	private static final int action_count = 3;
-	private static boolean[] finished = new boolean[action_count];
+	private static int[] status = new int[action_count];
+	/*
+	 * 0 for default
+	 * 1 for started
+	 * 2 for finished
+	 */
 	private static int index = 0;
 	
 	public static void init(){
 		goToAirShip = new Move(110);// put distance to airship
+		stopAtAirShip = new Stop(5000);
+		walkbackfromAirShip = new Move(22);
 		
 		for(int i=0;i<action_count;i++){
-			finished[i] = false;
+			status[i] = 0;
 		}
 		
 		goToAirShip.start();
+		status[0] = 0;
 	}
 	
 	public static void loop(){
-		if(index == 0){
+		
+		if(index == 0 && status[0] == 1){
 			goToAirShip.loop();
-			if(goToAirShip.isFinished()) index++;
+			if(goToAirShip.isFinished()){
+				status[index] = 2;
+				index++;
+			}
 		}
-		else if(index == 1){
-			
+		else if(index == 1 && status[0] == 2){
+			if(status[1] == 0){
+				stopAtAirShip.start();
+				if(stopAtAirShip.isFinished()){
+					status[index] = 2;
+					index++;
+				}
+			}
+		}
+		else if(index == 2 && status[1] == 2){
+			if(status[2] == 0){
+				walkbackfromAirShip.start();
+			}
+			walkbackfromAirShip.loop();
 		}
 		
 	}
