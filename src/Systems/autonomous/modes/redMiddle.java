1b0775aa1c1de799ce96b5f3794ea0e922e2c027
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-19 22:19:17 +0800

    redMiddle finished

--- src/Systems/autonomous/modes/redMiddle.java
+++ src/Systems/autonomous/modes/redMiddle.java
@@ -4,11 +4,11 @@ import Systems.autonomous.actions.*;
 
 public class redMiddle {
 	
-	private static Move goToAirShip,walkbackfromAirShip;
+	private static Move goToAirShip,walkbackfromAirShip,moveTobaseline,goOverbaseine;
 	private static Stop stopAtAirShip;
 	private static Rotate turnToBaseline1,turnToBaseline2;
 	
-	private static final int action_count = 3;
+	private static final int action_count = 7;
 	private static int[] status = new int[action_count];
 	/*
 	 * 0 for default
@@ -20,7 +20,13 @@ public class redMiddle {
 	public static void init(){
 		goToAirShip = new Move(110);// put distance to airship
 		stopAtAirShip = new Stop(5000);
-		walkbackfromAirShip = new Move(22);
+		walkbackfromAirShip = new Move(43);
+		turnToBaseline1 = new Rotate(-68);
+		moveTobaseline = new Move(108);
+		turnToBaseline2 = new Rotate(70);
+		goOverbaseine = new Move(57);
+		
+ 		
 		
 		for(int i=0;i<action_count;i++){
 			status[i] = 0;
@@ -42,17 +48,78 @@ public class redMiddle {
 		else if(index == 1 && status[0] == 2){
 			if(status[1] == 0){
 				stopAtAirShip.start();
-				if(stopAtAirShip.isFinished()){
-					status[index] = 2;
-					index++;
-				}
+				status[index] = 1;
+			}
+			
+			if(stopAtAirShip.isFinished()){
+				status[index] = 2;
+				index++;
 			}
 		}
 		else if(index == 2 && status[1] == 2){
 			if(status[2] == 0){
 				walkbackfromAirShip.start();
+				status[index] = 1;
 			}
 			walkbackfromAirShip.loop();
+			if(walkbackfromAirShip.isFinished()){
+				walkbackfromAirShip.done();
+				
+				status[index] = 2;
+				index++;
+				
+			}
+		}
+		else if(index == 3 && status[2] == 2){
+			if(status[3] == 0){
+				turnToBaseline1.start();
+				status[index] = 1;
+			}
+			turnToBaseline1.loop();
+			if(turnToBaseline1.isFinished()){
+				turnToBaseline1.done();
+				
+				status[index] = 2;
+				index++;
+			}
+		}
+		else if(index == 4 && status[3] == 2){
+			if(status[4] == 0){
+				moveTobaseline.start();
+				status[index] = 1;
+			}
+			moveTobaseline.loop();
+			if(moveTobaseline.isFinished()){
+				moveTobaseline.done();
+				
+				status[index] = 2;
+				index++;
+			}
+		}
+		else if(index == 5 && status[4] == 2){
+			if(status[5] == 0){
+				turnToBaseline2.start();
+				status[index] = 1;
+			}
+			turnToBaseline2.loop();
+			if(turnToBaseline2.isFinished()){
+				turnToBaseline2.done();
+				
+				status[index] = 2;
+				index++;
+			}
+		}
+		else if(index == 6 && status[5] == 2){
+			if(status[6] == 0){
+				goOverbaseine.start();
+			}
+			goOverbaseine.loop();
+			if(goOverbaseine.isFinished()){
+				goOverbaseine.done();
+				
+				status[index] = 2;
+				index++;
+			}
 		}
 		
 	}
