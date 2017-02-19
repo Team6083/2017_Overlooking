faad6080740ed2f66b71bc7c1160d701d4f25c53
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-19 17:57:37 +0800

    test_mode update

--- src/Systems/autonomous/modes/test_automode.java
+++ src/Systems/autonomous/modes/test_automode.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class test_automode {
 	
 	private static Rotate r1,r2;
-	private static Move m1;
+	private static Move m1,m2;
 	private static Stop s1;
 	
 	private static final int action_count = 3;
@@ -25,6 +25,7 @@ public class test_automode {
 		r2 = new Rotate(-90);
 		s1 = new Stop(3000);
 		m1 = new Move(20);
+		m2 = new Move(-30);
 		
 		for(int i=0;i<action_count;i++){
 			status[i] = 0;
@@ -43,31 +44,36 @@ public class test_automode {
 			m1.loop();
 			if(m1.isFinished()){
 				m1.done();
+				System.out.println("Task m1 had finished");
 				status[index] = 2;
 				index++;
+				
 			}
 		}
 		else if(index == 1 && status[0] == 2){
 			if(status[1] == 0){
 				s1.start();
 				if(s1.isFinished()){
+					System.out.println("Task s1 had finished");
 					status[index] = 2;
 					index++;
 				}
 			}
-		}/*
+		}
 		else if(index == 2){
 			if(status[2] == 0){
-				r2.start();
+				m2.start();
 				status[2] = 1;
 			}
-			r2.loop();
-			if(r2.isFinished()){
+			m2.loop();
+			if(m2.isFinished()){
+				System.out.println("Task m2 had finished");
 				status[2] = 2;
+				m2.done();
 				finished = true;
 				index++;
 			}
-		}*/
+		}
 		
 		
 		
