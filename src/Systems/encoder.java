66b389192ec98f3d99f331df39a8dc81853e8449
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-19 17:57:55 +0800

    更新參數

--- src/Systems/encoder.java
+++ src/Systems/encoder.java
@@ -11,12 +11,13 @@ public class encoder {
 	
 	private static final int wheel_circumference = 19;//needs to measure
 	
-	private static final int left_port = 1;
+	private static final int left_port = -1;
 	private static final int right_port = 0;
 	
-	private static int error_range = 10;
+	private static int error_range = 1000;
 	private static double max_speed = 0.3;
 	
+	private static boolean isTargetdistancel = false,isTargetdistancer = false;
 	public static boolean isTargetdistance = false;
 	public static double left_speed=0,right_speed=0;
 	
@@ -34,7 +35,7 @@ public class encoder {
 	}
 	
 	private static void walk_distence(){
-		error_step_left = left.get_distence() - target_step;
+		error_step_left = -left.get_distence() - target_step;
 		error_step_right = right.get_distence() - target_step;
 		
 		if((int) Math.abs(error_step_left) > error_range){
@@ -50,16 +51,20 @@ public class encoder {
 		if(Math.abs(left_speed) > max_speed){
 			if(left_speed > 0) left_speed = max_speed;
 			else left_speed = -max_speed;
-			isTargetdistance = false;
+			isTargetdistancel = false;
 		}
-		else if(left_speed == 0) isTargetdistance = true;
+		else if(left_speed == 0) isTargetdistancel = true;
 		
 		if(Math.abs(right_speed) > max_speed){
 			if(right_speed > 0) right_speed = max_speed;
 			else right_speed = -max_speed;
-			isTargetdistance = false;
+			isTargetdistancer = false;
 		}
-		else if(right_speed == 0) isTargetdistance = true;
+		else if(right_speed == 0) isTargetdistancer = true;
+		
+		if(isTargetdistancer&&isTargetdistancel) isTargetdistance = true;
+		else isTargetdistance = false;
+		left_speed = -left_speed;
 		
 	}
 	
@@ -72,7 +77,8 @@ public class encoder {
     
     private static void dashboard(){
 		SmartDashboard.putNumber("target_step",target_step);
-		SmartDashboard.putNumber("error_step_right_left", error_step_left);
+		SmartDashboard.putNumber("error_step_left", error_step_left);
+		SmartDashboard.putNumber("error_step_right", error_step_right);
 		SmartDashboard.putNumber("accumulation_step", left.get_accumulation_step());
     }
 }
