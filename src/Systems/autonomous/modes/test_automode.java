05a51ef9660b7413e97795cf6c21f24a39111a27
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-26 21:12:57 +0800

    test_automode bug fix

--- src/Systems/autonomous/modes/test_automode.java
+++ src/Systems/autonomous/modes/test_automode.java
@@ -8,9 +8,10 @@ public class test_automode {
 	private static Rotate r1,r2;
 	private static Move m1,m2;
 	private static Stop s1;
+	private static Action curraction;
 	
 	private static final int action_count = 3;
-	private static int[] status = new int[action_count];
+	private static boolean finish;
 	/*
 	 * 0 for default
 	 * 1 for started
@@ -20,64 +21,52 @@ public class test_automode {
 	
 	public static boolean finished = false;
 	
+	public static boolean isFinish(){
+		return finish;
+	}
+	
 	public static void init(){
+		if(finish) return;
 		r1 = new Rotate(180);
 		r2 = new Rotate(-90);
-		s1 = new Stop(3000);
-		m1 = new Move(20);
-		m2 = new Move(-30);
-		
-		for(int i=0;i<action_count;i++){
-			status[i] = 0;
-		}
+		s1 = new Stop(5000);
+		m1 = new Move(57);
+		m2 = new Move(-38);
 		
 		
-		m1.start();
-		status[0] = 1;
+		curraction = m1;
+		curraction.start();
+		finish = false;
 		SmartDashboard.putString("Control mode", "auto inited");
 		
 	}
 	
 	public static void loop(){
-		if(finished) return;
-		if(index == 0 && status[0] == 1){
-			m1.loop();
-			if(m1.isFinished()){
-				m1.done();
-				System.out.println("Task m1 had finished");
-				status[index] = 2;
-				index++;
-				
+		curraction.loop();
+		
+		if(curraction.isFinished()){
+			curraction.done();
+			System.out.println("Action "+index+" had finished.");
+			index++;
+			switch(index){
+			case 1:
+				curraction = s1;
+				break;
+			case 2:
+				curraction = m2;
+				break;
+			case 3:
+				finish = true;
+				break;
 			}
-		}
-		else if(index == 1 && status[0] == 2){
-			if(status[1] == 0){
-				s1.start();
-				if(s1.isFinished()){
-					System.out.println("Task s1 had finished");
-					status[index] = 2;
-					index++;
-				}
-			}
-		}
-		else if(index == 2){
-			if(status[2] == 0){
-				m2.start();
-				status[2] = 1;
-			}
-			m2.loop();
-			if(m2.isFinished()){
-				System.out.println("Task m2 had finished");
-				status[2] = 2;
-				m2.done();
-				finished = true;
-				index++;
-			}
+			
+			
+			SmartDashboard.putInt("Auto Status", index);
+			curraction.start();
 		}
 		
 		
 		
-		SmartDashboard.putInt("Auto Status", index);
 	}
 	
 	public static void finish(){
