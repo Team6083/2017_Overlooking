1fbe6224090417fa0d537d23770203ff907e8497
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-02-26 21:16:26 +0800

    putball action init

---
+++ src/Systems/autonomous/actions/putball.java
@@ -0,0 +1,29 @@
+package Systems.autonomous.actions;
+
+public class putball implements Action {
+
+	@Override
+	public void loop() {
+		
+
+	}
+
+	@Override
+	public void start() {
+		
+
+	}
+
+	@Override
+	public void done() {
+		
+
+	}
+
+	@Override
+	public boolean isFinished() {
+		
+		return false;
+	}
+
+}
