4dc63a0ce5ca7776ee8930f43a14596b3d38fd11
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-03-13 01:16:24 +1100

    add mode baseline

---
+++ src/Systems/autonomous/modes/baseline.java
@@ -0,0 +1,56 @@
+package Systems.autonomous.modes;
+
+import Systems.DriveBase;
+import Systems.autonomous.actions.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class baseline {
+	
+	private static Move goBaseline;
+	private static Stop stop;
+	
+	private static boolean finish;
+	
+	private static int index = 0;
+	private static Action curraction;
+	
+	public static boolean ifFinished(){
+		return finish;
+	}
+	
+	public static void init(){
+		goBaseline = new Move(93.3);// put distance to baseline
+		stop = new Stop(5000);
+		
+ 		
+		finish = false;
+		curraction = goBaseline;
+		curraction.start();
+	}
+	
+	public static void loop(){
+		curraction.loop();
+		
+		if(curraction.isFinished()){
+			curraction.done();
+			System.out.println("Action "+index+" had finished.");
+			index++;
+			switch(index){
+			case 1:
+				curraction = stop;
+				break;
+			case 2:
+				finish = true;
+				break;
+			}
+			SmartDashboard.putInt("Auto Status", index);
+			curraction.start();
+		}
+		
+	}
+	
+	public static void finish(){
+		DriveBase.input(0, 0);
+		DriveBase.drivabase_control();
+	}
+}
